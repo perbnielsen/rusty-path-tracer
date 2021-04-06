5fba23d73074a2126a7398839cb7af594eaef700
Per Bleshøy Nielsen <per.nielsen@productmadness.com>
2021-04-06 19:15:40 +0100

    Introduced translate on camera

--- src/camera.rs
+++ src/camera.rs
@@ -1,16 +1,21 @@
 use crate::viewport::Viewport;
 use cgmath::{Basis3, Point3, Rotation, Vector3};
 
+#[derive(Clone, Copy)]
 pub struct Camera {
-    pub basis: Basis3<f32>,
-    pub fov: f32,
-    pub origin: Point3<f32>,
+    basis: Basis3<f32>,
+    fov: f32,
+    origin: Point3<f32>,
 }
 
 impl Camera {
     pub fn new(origin: Point3<f32>, forward: Vector3<f32>, up: Vector3<f32>, fov: f32) -> Camera {
         let basis = Basis3::look_at(forward, up);
         Camera { basis, origin, fov }
+    }
+
+    pub fn translate(&mut self, vector: Vector3<f32>) {
+        self.origin += vector;
     }
 
     pub fn get_viewport(&self, width: usize, height: usize) -> Viewport {
